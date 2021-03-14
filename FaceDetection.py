47f30ac15ae2e0574d85585380efc7a898c3b0b0
RajrupDasid <rajrupdasid@outlook.com>
2021-03-14 16:55:25 +0530

    Facedetect

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,6 +1,17 @@
 import cv2 
 import numpy as np
+import pickle
 face_cascade = cv2.CascadeClassifier('C:/Users/Rajrup Das/source/repos/FaceDetection/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trained.yml")
+
+
+labels ={"person_name":1}
+with open ("labels.pickle",'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k  for k,v in og_labels.items()}
+
+
 
 cap = cv2.VideoCapture(0)
 while (True):
@@ -11,8 +22,25 @@ while (True):
         print(x,y,w,h)
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+        #recognize deep learning model
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf<=85:
+            print(id_)
+            print(labels[id_])
         img_item = "My-image.png"
         cv2.imwrite(img_item, roi_gray)
+
+        color = (255,0,0)
+        stroke = 2
+        
+        end_cord_x = x+y
+        end_cord_y = y+h
+
+        cv2.rectangle(frame, (x,y), (end_cord_x,end_cord_y), color, stroke)
+
+
+
+
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('e'):
         break
